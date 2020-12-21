cbc0081fd3642e15f7012455767f723ea080e9d7
grgr-dkrk <shayashi513@gmail.com>
2020-12-21 22:17:32 +0900

    nonested

--- src/rules/basic/no-nested-touchables.ts
+++ src/rules/basic/no-nested-touchables.ts
@@ -33,7 +33,7 @@ export const rule: Rule.RuleModule = {
             elementType(child) === BUTTON,
         )
 
-        if (!!clickableChild) {
+        if (clickableChild) {
           context.report({
             node,
             message:
