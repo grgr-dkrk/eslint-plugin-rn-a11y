de1c598a4c1247a744dd88b93bf8c0bc88f325d7
grgr-dkrk <shayashi513@gmail.com>
2020-12-29 21:57:48 +0900

    feat: get codecov coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  coverageReporters: ['lcov', 'html'],
-  coverageDirectory: 'reports',
   roots: ['<rootDir>/__tests__'],
   preset: 'ts-jest',
   testPathIgnorePatterns: ['<rootDir>/__tests__/createTesterOptions.ts'],
