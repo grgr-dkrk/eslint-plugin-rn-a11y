fa0f85f9b13c5fa5b766a0016de1b762eec6fdb2
grgr-dkrk <shayashi513@gmail.com>
2021-01-31 16:51:03 +0900

    feat: add "no-long-alt" rules

---
+++ __tests__/rules/basic/no-long-alt.test.ts
@@ -0,0 +1,64 @@
+import { RuleTester } from 'eslint'
+import { noLongAlt } from '../../../src/rules'
+import { createTesterOptions } from '../../createTesterOptions'
+
+const ruleTester = new RuleTester(createTesterOptions())
+
+ruleTester.run('no-long-alt', noLongAlt, {
+  valid: [
+    {
+      code: `<Image accessible source={require('test.jpg')} />`,
+    },
+    {
+      code: `<Image accessible accessibilityLabel="${'a'.repeat(
+        40,
+      )}" source={require('test.jpg')} />`,
+    },
+    {
+      code: `<Image accessible accessibilityLabel="${'a'.repeat(
+        125,
+      )}" source={require('test.jpg')} />`,
+    },
+    {
+      code: `<Image accessible accessibilityLabel="${'a'.repeat(
+        130,
+      )}" source={require('test.jpg')} />`,
+      options: [
+        {
+          limit: 130,
+        },
+      ],
+    },
+  ],
+  invalid: [
+    {
+      code: `<Image accessible accessibilityLabel="${'a'.repeat(
+        126,
+      )}" source={require('test.jpg')} />`,
+      errors: ['Alternate messages should be no more than 125 characters.'],
+    },
+    {
+      code: `<Image accessible accessibilityLabel="${'a'.repeat(
+        31,
+      )}" source={require('test.jpg')} />`,
+      errors: ['Alternate messages should be no more than 30 characters.'],
+      options: [
+        {
+          limit: 30,
+        },
+      ],
+    },
+    {
+      code: `<MyA11yImage accessible accessibilityLabel="${'a'.repeat(
+        31,
+      )}" source={require('test.jpg')} />`,
+      errors: ['Alternate messages should be no more than 30 characters.'],
+      options: [
+        {
+          Image: ['MyA11yImage'],
+          limit: 30,
+        },
+      ],
+    },
+  ],
+})
