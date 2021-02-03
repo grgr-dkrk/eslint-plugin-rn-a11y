ec2521f793811c78e79752a4b859951afd3d275f
grgr-dkrk <shayashi513@gmail.com>
2021-02-04 00:28:58 +0900

    fix(types): Fix definitions
    1. Fixed ast-utils type definition being invalid
    2. Removed the RuleTester instance from the type definition

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/__tests__'],
   preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'ts', 'd.ts'],
   testPathIgnorePatterns: ['<rootDir>/__tests__/createTesterOptions.ts'],
   testEnvironment: 'node',
 }
--- src/utils/ast/getStyleNames.ts
+++ src/utils/ast/getStyleNames.ts
@@ -1,6 +1,14 @@
 import { getProp } from 'jsx-ast-utils'
 import { STYLE } from '../../constants/props'
-import { JSXOpeningElement } from '../../types'
+import {
+  ArrayExpression,
+  ConditionalExpression,
+  JSXExpressionContainer,
+  JSXIdentifier,
+  JSXOpeningElement,
+  LogicalExpression,
+  MemberExpression,
+} from '../../types'
 
 /**
  * get all styles from node.attributes.
@@ -8,45 +16,51 @@ import { JSXOpeningElement } from '../../types'
 export const getStyleNames = (
   attributes: JSXOpeningElement['attributes'],
 ): string[] | void => {
-  const styleProp = getProp(attributes, STYLE)
+  const styleProp = getProp<JSXOpeningElement>(attributes, STYLE)
   if (!styleProp?.value) return
   const { value } = styleProp
   if (value.type === 'JSXExpressionContainer') {
+    const { expression } = (value as unknown) as JSXExpressionContainer
     // style={styles.wrapper}
-    if (value.expression?.type === 'MemberExpression') {
-      return [value.expression?.property.name]
+    if (expression.type === 'MemberExpression') {
+      return [(expression.property as JSXIdentifier).name]
     }
 
-    if (
-      value.expression?.type === 'ArrayExpression' &&
-      value.expression?.elements.length
-    ) {
-      return value.expression?.elements.reduce((acc, element) => {
-        // style={[styles.wrapper, styles.body]}
-        if (element.type === 'MemberExpression') {
-          return [...acc, element.property.name]
-        }
-
-        // style={[styles.wrapper, isActive ? styles.isActive : styles.isNotActive ]}
-        if (element.type === 'ConditionalExpression') {
-          const properties: string[] = []
-          if (element.consequent.type === 'MemberExpression') {
-            properties.push(element.consequent.property.name)
+    if (expression?.type === 'ArrayExpression') {
+      const arrayExpression = expression as ArrayExpression<JSXOpeningElement>
+      if (arrayExpression?.elements.length) {
+        return arrayExpression?.elements.reduce<string[]>((acc, element) => {
+          // style={[styles.wrapper, styles.body]}
+          if (element.type === 'MemberExpression') {
+            const { property } = (element as unknown) as MemberExpression
+            return [...acc, (property as JSXIdentifier).name]
           }
-          if (element.alternate.type === 'MemberExpression') {
-            properties.push(element.alternate.property.name)
-          }
-          return [...acc, ...properties]
-        }
 
-        // style={[styles.wrapper, isActive && styles.isActive ]}
-        if (
-          element.type === 'LogicalExpression' &&
-          element.right.type === 'MemberExpression'
-        ) {
-          return [...acc, element.right.property.name]
-        }
-      }, [])
+          // style={[styles.wrapper, isActive ? styles.isActive : styles.isNotActive ]}
+          if (element.type === 'ConditionalExpression') {
+            const {
+              consequent,
+              alternate,
+            } = (element as unknown) as ConditionalExpression
+            const properties: string[] = []
+            if (consequent.type === 'MemberExpression') {
+              properties.push((consequent.property as JSXIdentifier).name)
+            }
+            if (alternate.type === 'MemberExpression') {
+              properties.push((alternate.property as JSXIdentifier).name)
+            }
+            return [...acc, ...properties]
+          }
+
+          // style={[styles.wrapper, isActive && styles.isActive ]}
+          if (element.type === 'LogicalExpression') {
+            const { right } = (element as unknown) as LogicalExpression
+            if (right.type === 'MemberExpression') {
+              return [...acc, (right.property as JSXIdentifier).name]
+            }
+          }
+        }, [])
+      }
     }
   }
 }
