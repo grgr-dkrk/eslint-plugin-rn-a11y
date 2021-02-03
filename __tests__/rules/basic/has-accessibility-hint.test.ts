2ace338eeb6f0793cf5fa6c417bcfee407e7e230
grgr-dkrk <shayashi513@gmail.com>
2021-02-03 23:04:06 +0900

    feat: fix ESLint types(workaround)

--- __tests__/rules/basic/has-accessibility-hint.test.ts
+++ __tests__/rules/basic/has-accessibility-hint.test.ts
@@ -1,4 +1,4 @@
-import { RuleTester } from 'eslint'
+import { RuleTester } from '../../../src/types/modules/eslint'
 import { hasAccessibilityHint } from '../../../src/rules'
 import { createTesterOptions } from '../../createTesterOptions'
 
